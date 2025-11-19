c96403670610d56fcd03bb5e5cf17c18a9068254
Daniel Herênio <santosherenio76@gmail.com>
2025-11-19 18:13:37 -0300

    removendo imagem

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ export const Header = () => {
     return(
         <header className="flex justify-between items-center my-5 mx-3">
             <div className="flex items-center gap-3">
-                <Logo/>
+               
                 <ThemeTogle/>
 
             </div>
